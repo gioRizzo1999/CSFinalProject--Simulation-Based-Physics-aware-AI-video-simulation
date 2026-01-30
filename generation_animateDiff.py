53fcb5a8b918060dd103accd7fc4395fd6c2843b
Giovanni Rizzo <gioriz@MacBook-Pro.homenet.telecomitalia.it>
2026-01-30 14:50:41 +0100

    added imports and setup to animateDiff file

--- generation_animateDiff.py
+++ generation_animateDiff.py
@@ -3,3 +3,16 @@
 import os
 import torch
 from PIL import Image
+
+from diffusers import (
+    AnimateDiffControlNetPipeline,
+    ControlNetModel,
+    MotionAdapter,
+    DDIMScheduler,
+)
+from diffusers.utils import export_to_gif
+
+#setup for mps
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+dtype = torch.float32  
+
