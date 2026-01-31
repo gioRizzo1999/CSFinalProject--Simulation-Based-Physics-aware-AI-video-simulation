54f89f8955b746402e4c3359e9503e9a8c98f54c
Giovanni Rizzo <gioriz@MacBook-Pro.homenet.telecomitalia.it>
2026-01-31 16:04:33 +0100

    added load_control_maps_frames function

--- generation_animateDiff.py
+++ generation_animateDiff.py
@@ -1,18 +1,22 @@
-# animate diffusion on controlnet frames
-
-import os
 import torch
+import os
+from diffusers import AnimateDiffControlNetPipeline, ControlNetModel, MotionAdapter
+from diffusers.utils import export_to_gif
 from PIL import Image
 
-from diffusers import (
-    AnimateDiffControlNetPipeline,
-    ControlNetModel,
-    MotionAdapter,
-    DDIMScheduler,
-)
-from diffusers.utils import export_to_gif
 
-#setup for mps
+# setting backend
 device = "mps" if torch.backends.mps.is_available() else "cpu"
-dtype = torch.float32  
 
+# folders
+input_maps = "depth"
+output_path = "ouput.gif"
+resolution = (256, 240)
+frames_num = 8
+
+# load conditioning frames
+frames = sorted(os.listdir(input_maps))[:frames_num]
+input_frames = [
+    Image.open(os.path.join(input_maps, f)).convert("RGB").resize(resolution)
+    for f in frames
+]
